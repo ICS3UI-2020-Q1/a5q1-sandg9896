263bb482650dee2aa646365b57fca60a4e6beda7
sandg9896 <sandg9896@wrdsb.ca>
2020-10-06 13:27:18 +0000

    Multiplication Table

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * create a “multiplication table”
+ * @ Gavin 
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creaate a Scannner for user input
+    Scanner input = new Scanner(System.in);
+    // ask user to input number
+    System.out.println("Please enter a number");
+    
+    // variable for user input
+       int number = input.nextInt();
+    
+    // create for loop
+      for(int count = 1; count <= 12; count++){
+       
+       // create variable for multiplication table  
+       int answer = count * number;
+       
+       // loop action to print the number
+       System.out.println(count + "x" + number + "=" + answer);
+      }
+
     
   }
 }
